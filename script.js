de90085a25aac440be74027402689a468e8fb127
Jonas Neufeld <jonas@MacBook-Pro-3.local>
2023-11-02 09:38:09 -0500

    adding starting project

---
+++ script.js
@@ -0,0 +1,35 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector('.color1');
+var color2 = document.querySelector('.color2');
+var body = document.getElementById("gradient");
+var randomButton = document.getElementById("random-button")
+
+color1.value = "#ff0000";
+color2.value = "#ffff00";
+body.value
+
+css.textContent = getComputedStyle(body).backgroundImage + ";";
+
+function setGradient() {
+  body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+
+  css.textContent = body.style.background + ";";
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF'
+  var color = '#'
+  for (var i=0; i<6; i++) {
+    color += letters[Math.floor(Math.random() *16)]
+  }
+  return color;
+}
+
+
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
+randomButton.addEventListener("click", function() {
+  color1.value = getRandomColor();
+  color2.value = getRandomColor();
+  setGradient();
+})
